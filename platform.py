8763d100930cbeaf43b22367686f341698307a37
Stathis Stamoulis <sstamoulis@mosystems.biz>
2019-08-26 12:31:38 +0300

    Fix v3 toolchain reference in manifest

--- platform.py
+++ platform.py
@@ -24,7 +24,7 @@ class Espressif8266Platform(PlatformBase):
         if "buildfs" in targets:
             self.packages['tool-mkspiffs']['optional'] = False
         if "esp8266-rtos-sdk-v3" in framework:
-            self.packages['toolchain-xtensa-v3']['optional'] = False
+            self.packages['toolchain-xtensa']['version'] = "5.2.0"
         return PlatformBase.configure_default_packages(
             self, variables, targets)
 
